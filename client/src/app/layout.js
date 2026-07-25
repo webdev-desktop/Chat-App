c2da5ca44420eec03d60ff88ba85d5bddb501442
Apurv <webdev.desktop01@gmail.com>
2026-07-25 14:52:51 +0530

    Remove default Next.js starter font and clean initial setup

--- client/src/app/layout.js
+++ client/src/app/layout.js
@@ -7,10 +7,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full`}
-    >
+    <html lang="en" className={"h-full"}>
       <body>{children}</body>
     </html>
   );
