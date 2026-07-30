5bd52d385d961986a7efda769efdb94a3d6eb131
Apurv <webdev.desktop01@gmail.com>
2026-07-30 21:12:38 +0530

    feat(auth): Remove All User Data Route

--- server/src/controllers/user/profile.controller.js
+++ server/src/controllers/user/profile.controller.js
@@ -3,16 +3,6 @@ import UserModel from "../../models/user.js";
 import ErrorHandler from "../../utils/ErrorHandler.js";
 import userResponse from "../../utils/userResponse.js";
 
-export const allUsers = async (req, res, next) => {
-  try {
-    const users = await UserModel.find({ _id: { $ne: req.user._id } });
-    if (!users) return next(new ErrorHandler("User not found.", 404));
-    userResponse(res, 200, user, "All Users List");
-  } catch (error) {
-    next(error);
-  }
-};
-
 export const findUser = async (req, res, next) => {
   try {
     const { username } = validateInput(req, "find");
